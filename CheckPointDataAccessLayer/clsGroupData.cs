36c4035480cfd941c3bd91668e9c761bba5d6cf3
Mohammad <Mohammad@DESKTOP-FDC27QR>
2025-12-30 18:04:44 +0400

    Add SQLite support and group data access layer
    Added System.Data.SQLite via NuGet and updated project files for integration. Introduced clsGroupData.cs with methods to retrieve group info by ID or name from the Groups table. Updated packages.config and project references accordingly.

---
+++ CheckPointDataAccessLayer/clsGroupData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPointDataAccessLayer
+{
+    public class clsGroupData
+    {
+        private static readonly string ConnectionString = clsDataAccessSettings.ConnectionString;
+        public static bool GetGroupByID(int GroupID, ref string GroupName)
+        {
+            bool IsFound = false;
+
+            string Query = "SELECT * FROM Groups WHERE GroupID = @GroupID";
+
+            using (var Connection = new SQLiteConnection(ConnectionString))
+            using (var Command = new SQLiteCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@GroupID", GroupID);
+
+                Connection.Open();
+
+                using (var Reader = Command.ExecuteReader())
+                {
+                    if (Reader.Read())
+                    {
+                        IsFound = true;
+
+                        GroupName = Reader["GroupName"].ToString();
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+        public static bool GetGroupByName(string GroupName, ref int GroupID)
+        {
+            bool IsFound = false;
+
+            string Query = "SELECT * FROM Groups WHERE GroupName = @GroupName";
+
+            using (var Connection = new SQLiteConnection(ConnectionString))
+            using (var Command = new SQLiteCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@GroupName", GroupName);
+
+                Connection.Open();
+
+                using (var Reader = Command.ExecuteReader())
+                {
+                    if (Reader.Read())
+                    {
+                        IsFound = true;
+
+                        GroupID = Convert.ToInt32(Reader["GroupID"]);
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+
+    }
+}
