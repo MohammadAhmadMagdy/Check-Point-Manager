c078e75f16502eed9706be75e1fa4514c38af666
Mohammad <Mohammad@DESKTOP-FDC27QR>
2025-12-31 15:24:01 +0400

    Add business layer and Excel support; update dependencies
    Added CheckPointBusinessLayer project with clsGroup and clsItem classes for business logic. Integrated Excel file handling via ClosedXML and related packages. Updated solution and project references, added required NuGet dependencies, and configured assembly binding redirects for compatibility.

---
+++ CheckPointBusinessLayer/clsGroup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPointBusinessLayer
+{
+    public class clsGroup
+    {
+        public enum enMode { AddNew = 0, Update = 1 }
+        public enMode Mode = enMode.AddNew;
+
+        public int GroupID {  get; set; }
+        public string GroupName { get; set; }
+
+        public clsGroup()
+        {
+            this.GroupID = -1;
+            this.GroupName = "";
+
+            Mode = enMode.AddNew;
+        }
+        public clsGroup(int GroupID, string GroupName)
+        {
+            this.GroupID = GroupID;
+            this.GroupName = GroupName;
+
+            Mode = enMode.Update;
+        }
+
+    }
+}
---
+++ CheckPointBusinessLayer/clsItem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using CheckPointDataAccessLayer;
+
+namespace CheckPointBusinessLayer
+{
+    public class clsItem
+    {
+        public enum enMode { AddNew = 0, Update = 1}
+        public enMode Mode = enMode.AddNew;
+        public int ItemCode {  get; set; }
+        public string Description { get; set; }
+        public int Qty {  get; set; }
+        public int LzQty {  get; set; }
+        public decimal RetailPrice {  get; set; }
+
+        public clsItem()
+        {
+            this.ItemCode = -1;
+            this.Description = "";
+            this.Qty = 0;
+            this.LzQty = 0;
+            this.RetailPrice = 0;
+
+            Mode = enMode.AddNew;
+        }
+        private clsItem(int ItemCode, string Description, int Qty, int LzQty, decimal RetailPrice)
+        {
+            this.ItemCode = ItemCode;
+            this.Description = Description;
+            this.Qty = Qty;
+            this.LzQty = LzQty;
+            this.RetailPrice = RetailPrice;
+
+            Mode = enMode.Update;
+        }
+
+        public static List<int> UpdateStockAndGetNewItemsCodes(string ExcelPath)
+        {
+            return clsItemDataAccess.UpdateItemsFromExcel(ExcelPath);
+        }
+        public static DataTable GetAllStockList()
+        {
+            return clsItemDataAccess.GetAll();
+        }
+        public static clsItem Find(int ItemCode)
+        {
+            int Qty = 0, LzQty = 0;
+            string Description = "";
+            decimal RetailPrice = 0;
+
+            if (clsItemDataAccess.GetByItemCode(ItemCode, ref Description, ref Qty, ref LzQty, ref RetailPrice))
+            {
+                return new clsItem(ItemCode, Description, Qty, LzQty, RetailPrice);
+            }
+            else
+                return null;
+        }
+
+
+    }
+}
